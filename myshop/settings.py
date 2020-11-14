b9460941739726a5381a442fb20784f7592d048b
marcosgainza <35632171+marcosgainza@users.noreply.github.com>
2020-11-14 18:38:40 -0300

    modificacion archivo setting

--- myshop/settings.py
+++ myshop/settings.py
@@ -76,14 +76,20 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.mysql',
+   #     'NAME': 'myshop',
+    #    'USER': 'root',
+     #   'PASSWORD': '',
+      #  'HOST': 'localhost',
+       # 'PORT': '3306',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'myshop',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
